78dd087e65c6c295a6a0b38901004d956c8d8d07
LeeMotaung <motaunglerato12@gmail.com>
2023-07-16 13:39:18 -0700

    ./2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main- prints the alphabet in lowercase*/
-/* Return-Always 0(success) */
+/**
+* main - entry point
+*
+*Return: always 0 (sucess)
+*/
 int main(void)
 {
 char lowercase = 'a';
