d61ea217c515cad335de69c3ac3728194eb6082b
LeeMotaung <motaunglerato12@gmail.com>
2023-07-16 14:15:23 -0700

    ./6-print_numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+*Return: always 0 (sucess)
+*/
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+printf('\n');
+return (0);
+}
